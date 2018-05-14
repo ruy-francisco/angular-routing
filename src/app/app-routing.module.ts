e7352e04d3155f3fec7e2888da44c59faf5d5d08
ruy-francisco <ruy.fsalmeida@gmail.com>
2018-05-14 16:51:49 -0300

    Learning Build techniques

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerComponent } from './customer/customer.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'customer', component: CustomerComponent }
+  { path: 'customer', component: CustomerComponent },
+  { path: 'admin', component: AdminComponent },
 ];
 
 @NgModule({
